87d5fd0f2cb6e8aea7853ebd085d2d17bb926964
misoi <cytnhia.chepkemoi@gmail.com>
2016-05-25 19:11:42 +0300

    created two activities one button linking to a page to that displays candidates in a list

---
+++ Listview/app/src/main/java/com/example/cynthia/listview/ListActivity.java
@@ -0,0 +1,16 @@
+package com.example.cynthia.listview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cynthia on 5/25/16.
+ */
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list);
+    }
+}
